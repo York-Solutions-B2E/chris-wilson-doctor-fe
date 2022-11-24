d73af6f8f058d79c5f3119ac065296cf5c858bb1
theNewbieFiles <chris.wilson@chrispcr.com>
2022-11-23 17:38:39 -0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { TestComponent } from './components/test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent}, 
+  {path: "test", component: TestComponent},
+  { path: '**', component: HomeComponent } //TODO: change this to a 404
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
